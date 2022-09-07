aa4cfca4b8e2a9d20a229869700b8ec4d76ab7ed
Erik Drysdale <alla_hack@hotmail.com>
2022-09-07 16:38:30 -0700

    l1 approx with log(1+exp(-beta)) is garbage; using sqrt(x^2+eps)

--- tests/l1_approx.R
+++ tests/l1_approx.R
@@ -12,6 +12,18 @@ x = as.matrix(MASS::Boston[,-14])
 x_trans = scale(x)
 sprintf('Sum y=%.1f, sum x_trans^2=%.1f',sum(y),sum(x_trans**2))
 
+# (ii) Fit unregularized model
+mdl = glmnet(x=x_trans,y=y,family='binomial',alpha=1, lambda=0,standardize=FALSE)
+round(c(mdl$a0,mdl$beta[,'s0']),3)
+
+# (iii) Fit L2-only model
+mdl = glmnet(x=x_trans,y=y,family='binomial',alpha=0, lambda=1,standardize=FALSE)
+round(c(mdl$a0,mdl$beta[,'s0']),3)
+
+# (iii) Fit L1-only model
+mdl = glmnet(x=x_trans,y=y,family='binomial',alpha=1, lambda=0,standardize=FALSE)
+round(c(mdl$a0,mdl$beta[,'s0']),3)
+
 # Fit elastic net model for specific hyperparameter
 mdl = glmnet(x=x_trans,y=y,family='binomial',alpha=0.5, lambda=c(0.1),standardize=FALSE)
 abhat = c(as.numeric(mdl$a0),as.vector(mdl$beta))
@@ -21,3 +33,4 @@ print(df)
 # Coefficient values to be parsed in python
 paste(as.character(round(df$val,5)),collapse=', ')
 
+glm(y ~ x_trans, family=binomial)$coef
--- tests/l1_approx.py
+++ tests/l1_approx.py
@@ -7,7 +7,59 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
 from sklearn.datasets import load_boston
-from sklearn.linear_model import LogisticRegression
+
+# (ii) Fit with approximate loss
+def loss_g1(beta:np.ndarray, lam:np.ndarray or float, alpha:float, eps:float=1e-5) -> float:
+    """|x_i| ~ sqrt(x_i**2 + eps)"""
+    l1_approx = np.sqrt(beta**2 + eps)
+    ll = np.sum(lam * (alpha*l1_approx + 0.5*(1-alpha)*beta**2))
+    return ll
+
+def grad_g1(beta:np.ndarray, lam:np.ndarray or float, alpha:float, eps:float=1e-5)  -> np.ndarray:
+    """|x_i| ~ sqrt(x_i**2 + eps)"""
+    l1_approx = beta/np.sqrt(beta**2 + eps)
+    grad = lam * (alpha * l1_approx + (1-alpha)*beta)
+    return grad
+
+def loss_g2(beta:np.ndarray, lam:np.ndarray or float, alpha:float) -> float:
+    """|x_i| ~ [log(1+exp(-eps*x)) + log(1+exp(eps*x))]/eps"""
+    l1_approx = np.log(1 + np.exp(-beta)) + np.log(1 + np.exp(beta))
+    ll = np.sum(lam * (alpha*l1_approx + 0.5*(1-alpha)*beta**2))
+    return ll
+
+def grad_g2(beta:np.ndarray, lam:np.ndarray or float, alpha:float, eps:float=1e-5) -> np.ndarray:
+    l1_approx = 1/(1+np.exp(-beta)) - 1/(1+np.exp(beta))
+    grad = lam * (alpha * l1_approx + (1-alpha)*beta)
+    return grad
+
+
+b = np.arange(0.1,1,0.1)
+loss_g1(b, lam=1, alpha=1)
+loss_g2(b, lam=1, alpha=1)
+
+grad_g1(b, lam=1, alpha=1)
+grad_g2(b, lam=1, alpha=1)
+
+
+di_g = {1:{'loss':loss_g1, 'grad':grad_g1}, 2:{'loss':loss_g2, 'grad':grad_g2}}
+
+def neg_loglik(beta:np.ndarray, x:np.ndarray, y:np.ndarray, lam:np.ndarray or float, alpha:float, di:dict, eps:float=1e-5):
+    eta = x.dot(beta)
+    py = 1 / (1 + np.exp(-eta))
+    lik = -np.mean(y*np.log(py) + (1-y)*np.log(1-py))
+    reg = di['loss'](beta, lam, alpha, eps)
+    nll = lik + reg
+    return nll
+
+def grad_loglik(beta:np.ndarray, x:np.ndarray, y:np.ndarray, lam:np.ndarray or float, alpha:float, di:dict, eps:float=1e-5):
+    n = x.shape[0]
+    eta = x.dot(beta)
+    py = 1 / (1 + np.exp(-eta))
+    res = (y - py).reshape([n,1])
+    grad_lik = -x.T.dot(res).flatten() / n
+    grad_reg = di['grad'](beta, lam, alpha, eps)
+    grad = grad_lik + grad_reg
+    return grad
 
 
 # (i) Load and normalize data
@@ -17,25 +69,22 @@ x_trans = (x - np.atleast_2d(x.mean(0)))/np.atleast_2d(x.std(0,ddof=1))
 print(f'Sum y={y.sum():.1f}, sum x_trans^2={(x_trans**2).sum():.1f}')
 ix = np.c_[np.ones(len(x)), x_trans]
 
-# (ii) Fit with approximate loss
-def log_lik(beta, x, y):
-    eta = x.dot(beta)
-    py = 1 / (1 + np.exp(-eta))
-    nll = -np.mean(y*np.log(py) + (1-y)*np.log(1-py))
-    return nll
+# (ii) Fit un-regularized model
+x0_init = np.zeros(ix.shape[1])
+neg_loglik(beta=x0_init, x=ix, y=y, lam=0, alpha=1, di=di_g[1])
+grad_loglik(beta=x0_init, x=ix, y=y, lam=0, alpha=1, di=di_g[1])
+lam, alpha = 0, 1
+minimize(neg_loglik, x0_init, (ix, y, lam, alpha, di_g[1]), 'l-bfgs-b', grad_loglik).x.round(3)
 
-def grad_f(beta, x, y):
-    n = x.shape[0]
-    eta = x.dot(beta)
-    py = 1 / (1 + np.exp(-eta))
-    res = (y - py).reshape([n,1])
-    grad = -x.T.dot(res).flatten() / n
-    return grad
+# (iii) Fit l2-regularized model
+lam, alpha = np.append([0],np.repeat(1,x.shape[1])), 0
+minimize(neg_loglik, x0_init, (ix, y, lam, alpha, di_g[1]), 'l-bfgs-b', grad_loglik).x.round(3)
 
-def grad_g1(beta, lam, alpha, eps:float=1e-5):
-    """|x_i| ~ sqrt(x_i**2 + eps)"""
-    grad = lam * (alpha * (2*beta/np.sqrt(beta**2 + eps)) + (1-alpha)*beta)
-    return grad
+# (iv) Fit the l1-regularized model
+lam, alpha = np.append([0],np.repeat(0.1,x.shape[1])), 1
+minimize(neg_loglik, x0_init, (ix, y, lam, alpha, di_g[1], 1e-10), 'l-bfgs-b', grad_loglik).x.round(3)
+minimize(neg_loglik, x0_init, (ix, y, lam, alpha, di_g[2], 1e-10), 'l-bfgs-b', grad_loglik).x.round(3)
+
 
 
 # Compare to R-glmnet
@@ -44,14 +93,10 @@ r_coef = pd.Series(r_coef.split(', ')).astype(float)
 #pd.DataFrame({'py':py_coef, 'r':r_coef})
 
 
-# # This aligns with glm() in R
-# x0_init = np.zeros(ix.shape[1])
-# log_lik(x0_init, ix, y)
-# grad_f(x0_init, ix, y)
-# minimize(log_lik, x0_init, (ix, y), 'l-bfgs-b', grad_f).x
 
 
 # # (ii) Fit using sklearn
+# from sklearn.linear_model import LogisticRegression
 # lam_r = 0.1
 # alpha = 0.5
 # n = len(x)
