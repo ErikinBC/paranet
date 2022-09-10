e9da48cfd9b42ea4304e4211b0e02d4dc263846b
Erik Drysdale <alla_hack@hotmail.com>
2022-09-09 23:25:33 -0400

    preparing for python package

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='paranet',
+    version='0.1.0',    
+    description='paranet package',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ErikinBC/paranet',
+    author='Erik Drysdale',
+    author_email='erikinwest@gmail.com',
+    license='GPLv3',
+    license_files = ('LICENSE.txt'),
+    packages=['paranet'],
+    package_data={'paranet': ['tests/*', 'examples/*']},
+    include_package_data=True,
+    install_requires=['numpy', 'pandas', 'scipy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+    ],
+)
