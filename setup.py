0e24edff8639c1d70b7dbe08788d6d191c5f202d
Erik Drysdale <alla_hack@hotmail.com>
2022-09-12 23:29:00 -0400

    working version on test.pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='paranet',
-    version='0.1.3',    
+    version='0.1.4',    
     description='paranet package',
     long_description=README,
     long_description_content_type="text/markdown",
